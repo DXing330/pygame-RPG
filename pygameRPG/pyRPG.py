fb7482f13c91ef0babd11199fe72f427e827fc2d
DXing330 <105528351+DXing330@users.noreply.github.com>
2022-07-21 21:33:33 -0700

    Update pyRPG.py

--- pygameRPG/pyRPG.py
+++ pygameRPG/pyRPG.py
@@ -2,7 +2,10 @@ import pygame
 import os
 import sys
 import json
+import copy
+import random
 sys.path.append(".")
+sys.path.append("./pygame_functions")
 sys.path.append("./RPG2v3/RPG2v3_functions")
 sys.path.append("./RPG2v3/RPG2v3_functions/RPG2v3_quest")
 sys.path.append("./RPG2v3/RPG2v3_functions/RPG2v3_def")
@@ -15,6 +18,7 @@ import rpg2_city_function as city_func
 import rpg2_party_management_functions as party_func
 import rpg2_monster_function as monster_func
 import rpg2_battle_phase_function as battle_func
+import rpg2_player_action_function as player_act_func
 import rpg2_mage_tower_function as magetower_func
 import rpg2_save_function as save_func
 import rpg2_random_boss_function as boss_func
@@ -29,7 +33,9 @@ from rpg2_constant_quests import Q_Constants
 Q = Q_Constants()
 from pygconstants import PYGConstants
 P = PYGConstants()
-
+import draw_functions as draw_func
+#clock
+clock = pygame.time.Clock()
 #makes fonts
 pygame.font.init()
 #makes sound
@@ -44,77 +50,113 @@ FOREST_RAW = pygame.image.load(os.path.join("Assets", "forest.png"))
 FOREST_IMG = pygame.transform.scale(FOREST_RAW, (P.WIDTH, P.HEIGHT))
 CITY_RAW = pygame.image.load(os.path.join("Assets", "city.png"))
 CITY_IMG = pygame.transform.scale(CITY_RAW, (P.WIDTH, P.HEIGHT))
-HERO_RAW = pygame.image.load(os.path.join("Assets", "hero.png"))
-HERO_IMG = pygame.transform.scale(HERO_RAW, (P.SPRITE_WIDTH, P.SPRITE_HEIGHT))
-SUMMONER_RAW = pygame.image.load(os.path.join("Assets", "summoner.png"))
-SUMMONER_IMG = pygame.transform.scale(SUMMONER_RAW, (P.SPRITE_WIDTH, P.SPRITE_HEIGHT))
-KNIGHT_RAW = pygame.image.load(os.path.join("Assets", "knight.png"))
-KNIGHT_IMG = pygame.transform.scale(KNIGHT_RAW, (P.SPRITE_WIDTH, P.SPRITE_HEIGHT))
-HUNTER_RAW =pygame.image.load(os.path.join("Assets", "hunter.png"))
-HUNTER_IMG = pygame.transform.scale(HUNTER_RAW, (P.SPRITE_WIDTH, P.SPRITE_HEIGHT))
-ANGEL_RAW = pygame.image.load(os.path.join("Assets", "angel.png"))
-ANGEL_IMG = pygame.transform.scale(ANGEL_RAW, (P.SPRITE_WIDTH//2, P.SPRITE_HEIGHT//2))
-
-#function that will draw characters in battle
-def draw_heroes(h_p, h_ally):
-        for player in h_p:
-                if "Hunter" in player.name:
-                        WIN.blit(HUNTER_IMG,
-                                 (P.WIDTH - (P.SPRITE_WIDTH * 4),
-                                 P.HEIGHT - (P.SPRITE_HEIGHT * 4)))
-                if "Hero" in player.name:
-                        WIN.blit(HERO_IMG,
-                                 (P.WIDTH - (P.SPRITE_WIDTH * 2),
-                                 P.HEIGHT - (P.SPRITE_HEIGHT * 2)))
-                if "Summoner" in player.name:
-                        WIN.blit(SUMMONER_IMG,
-                                 (P.WIDTH - (P.SPRITE_WIDTH * 3),
-                                 P.HEIGHT - (P.SPRITE_HEIGHT * 3)))
-                if "Knight" in player.name:
-                        WIN.blit(KNIGHT_IMG,
-                                 (P.WIDTH - (P.SPRITE_WIDTH * 5),
-                                 P.HEIGHT - (P.SPRITE_HEIGHT * 5)))
+SPACE_RAW = pygame.image.load(os.path.join("Assets", "space.png"))
+SPACE_IMG = pygame.transform.scale(SPACE_RAW, (P.WIDTH, P.HEIGHT))
                 
-        for aly in h_ally:
-                if "Angel" in aly.name:
-                        WIN.blit(ANGEL_IMG,
-                                 (P.WIDTH - (P.SPRITE_WIDTH * 2),
-                                 P.HEIGHT - (P.SPRITE_HEIGHT * 3)))
-
-        pygame.display.update()
+#function that controls a hero attacking a monster
+def hero_attack(hero, h_p, m_p, h_wpn, h_amr):
+        #the game will wait while the hero picks
+        pick = True
+        while pick:
+                WIN.blit(FOREST_IMG, P.ORIGIN)
+                draw_func.draw_heroes(new_h_p, new_h_ally)
+                draw_func.draw_monsters(m_p)
+                draw_func.draw_monster_menu_list(m_p)
+                pygame.display.update()
+                clock.tick(P.SLOWFPS)
+                for event in pygame.event.get():
+                        if event.type == pygame.KEYDOWN:
+                                #need a way to pick a monster
+                                if event.key == pygame.K_1:
+                                        mon = m_p[0]
+                                        player_act_func.player_attack(hero, mon, h_wpn,
+                                                                      h_amr, h_p, m_p)
+                                        pick = False
+                                elif event.key == pygame.K_2:
+                                        mon = m_p[1]
+                                        player_act_func.player_attack(hero, mon, h_wpn,
+                                                                      h_amr, h_p, m_p)
+                                        pick = False
                 
+#function that controls the turns in battle
+def hero_turn(hero, h_p, m_p, h_ally, h_bag,
+              h_magic, h_wpn, h_amr):
+        clock = pygame.time.Clock()
+        turn = True
+        while turn:
+                clock.tick(P.SLOWFPS)
+                draw_func.draw_battle_menu(h_p, m_p, h_ally, h_magic,
+                                 h_wpn, h_amr)
+                pygame.display.update()
+                for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                                game = False
+                                pygame.quit()
+                        if event.type == pygame.KEYDOWN:
+                                if event.key == pygame.K_a and len(m_p) > 0:
+                                        hero_attack(hero, h_p, m_p, h_wpn, h_amr)
+
 
 #function that will control the battle
 def battle(h_p, m_p, h_ally, h_bag,
            h_magic, h_wpn, h_amr):
+        new_h_p = []
+        new_h_ally = []
+        new_h_wpn = []
+        new_h_amr = []
+        for hero in h_p:
+                copy_hero = copy.copy(hero)
+                new_h_p.append(copy_hero)
+        for ally in h_ally:
+                copy_ally = copy.copy(ally)
+                new_h_ally.append(copy_ally)
+        for wpn in h_wpn:
+                copy_weapon = copy.copy(wpn)
+                new_h_wpn.append(copy_weapon)
+        for amr in h_amr:
+                copy_armor = copy.copy(amr)
+                new_h_amr.append(copy_armor)
+        new_m_p = list(m_p)
         clock = pygame.time.Clock()
         battle = True
         while battle:
-                clock.tick(P.FPS)
+                clock.tick(P.SLOWFPS)
                 WIN.fill(P.WHITE)
                 WIN.blit(FOREST_IMG, P.ORIGIN)
-                draw_heroes(h_p, h_ally)
-                draw_battle_menu(h_p, m_p, h_ally, h_magic,
-                                 h_wpn, h_amr)
+                draw_func.draw_heroes(new_h_p, new_h_ally)
+                draw_func.draw_monsters(m_p)
                 pygame.display.update()
-                
-#function will list options for the player
-def draw_menu():
-        attack_text = REG_FONT.render("ATTACK monsters: A", 1, P.RED)
-        WIN.blit(attack_text, (P.WIDTH//2 - attack_text.get_width(), P.PADDING))
-        save_text = REG_FONT.render("RECORD journey: R", 1, P.BLACK)
-        WIN.blit(save_text, (P.WIDTH//2 - save_text.get_width(),
-                             P.PADDING + save_text.get_height()))
-        city_text = REG_FONT.render("return to CITY: C", 1, P.BLACK)
-        WIN.blit(city_text, (P.WIDTH//2 - city_text.get_width(),
-                             P.PADDING + city_text.get_height() * 2))
-        
+                for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                                game = False
+                                pygame.quit()
+                for hero in new_h_p:
+                        hero_turn(hero, new_h_p, new_m_p, new_h_ally, h_bag,
+                                  h_magic, new_h_wpn, new_h_amr)
+                for ally in new_h_ally:
+                        #maybe later we can make this more animated
+                        player_act_func.pet_action(new_h_ally, new_h_p, new_m_p)
+                for mon in new_m_p:
+                        if mon.health > 0:
+                                hero = party_func.pick_random_healthy_hero(new_h_p)
+                                monster_func.monster_attack(monster, hero, new_h_a, new_h_p, new_m_p)
+                for m in range(0, len(new_m_p)):
+                        for mon in new_m_p:
+                                if mon.health <= 0:
+                                        m_p.remove(mon)
+                for h in range(0, len(new_h_p)):
+                        for hero in new_h_p:
+                                if hero.health <= 0:
+                                        h_p.remove(hero)
+                if len(new_h_p) == 0 or len(new_m_p) == 0:
+                        battle = False
+
 
 #function will make the plains background        
 def draw_plains():
         WIN.fill(P.WHITE)
         WIN.blit(PLAINS_IMG, P.ORIGIN)
-        draw_menu()
+        draw_func.draw_menu()
         pygame.display.update()
         
 #function that will make the city
@@ -133,9 +175,9 @@ def RPG(h_party, h_magic, h_bag, h_ally, h_wpn, h_amr, quest, access):
         m_p = []
         game = True
         while game:
-                clock.tick(P.FPS)
+                clock.tick(P.SLOWFPS)
                 draw_plains()
-                draw_heroes(h_party, h_ally)
+                draw_func.draw_heroes(h_party, h_ally)
                 for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                                 game = False
@@ -177,6 +219,7 @@ def Start():
         while start:
                 start_text = REG_FONT.render("CONTINUE/START? C/S? ", 1, P.WHITE)
                 WIN.fill(P.BLACK)
+                WIN.blit(SPACE_IMG, P.ORIGIN)
                 WIN.blit(start_text, ((P.WIDTH - start_text.get_width())//2,
                                       P.PADDING))
                 pygame.display.update()
